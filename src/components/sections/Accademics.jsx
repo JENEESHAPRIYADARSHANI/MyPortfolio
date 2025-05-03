774fcd4f756163e81d39b4a513db89e665f01e23
jeneesha Priyadarshani <jjeneesha@students.nsbm.ac.lk>
2025-05-03 22:34:27 +0530

    Update GPA

--- src/components/sections/Accademics.jsx
+++ src/components/sections/Accademics.jsx
@@ -97,7 +97,7 @@ export const Accademics = () => {
               Student ID: <span className="text-blue-300">26969</span>
             </p>
             <p className="text-sm sm:text-xl font-bold text-blue-400 mt-2">
-              GPA: 3.85
+              GPA: 3.82
             </p>
           </div>
         </div>
